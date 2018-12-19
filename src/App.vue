896076924cfc77086f8b41019064d0c1436b47f3
omar-nav <omar.navarro@g.austincc.edu>
2018-12-19 16:55:49 -0600

    Webpack and vue-cli

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <message>Que pedo</message>
+    <message>Que onda</message>
+  </div>
+</template>
+
+<script>
+import Message from "./components/Message.vue";
+
+export default {
+  name: "app",
+  components: {
+    Message
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="box is-info">
+    <p>
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.box {
+  background: gray;
+  padding: 10px;
+  border: 1px solid darkgray;
+  margin-bottom: 1em;
+}
+</style>
